5c0329b93fc21ee2aff56cddd0fe441b68d8f32d
Dhananjay Kadam <jaykadam90@gmail.com>
2021-01-10 20:28:18 +0530

    chore: add title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Carousel from './components/carousel/carousel';
 function App() {
   return (
     <div className="App">
+      <div className="project-title">
+        <h2>Sample Carousel for PayPal</h2>
+      </div>
       <Carousel></Carousel>
     </div>
   );
